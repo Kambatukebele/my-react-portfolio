f0e790b6aacedcacd12bf5e7d29e4a4b61b7957b
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-10-31 12:16:01 +0100

    updated MenuNavigation

--- src/components/navigation/MenuNavigation.jsx
+++ src/components/navigation/MenuNavigation.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
-import NavLinks from '../NavLinks'
+import navLinks from '../NavLinks'
 
 function MenuNavigation({ isOpenCloseMenu, toggle }) {
     const menuVisibilityClass = isOpenCloseMenu ? 'block' : 'hidden';
   return (   
     <>
         <ul className={`absolute z-50 h-fit bg-gray-50 w-full top-9 lg:flex lg:w-96 lg:justify-between lg:items-center lg:static ${menuVisibilityClass}`}>
-        <li className="block my-4 text-center text-sm">
-            <a className="uppercase text-blue-700 font-medium" href="#">Home</a>
-        </li>
-        <li className="block my-4 text-center text-sm">
-            <a className="uppercase text-blue-700 font-medium" href="#">Portfolio</a>
-         </li>
-        <li className="block my-4 text-center text-sm">
-            <a className="uppercase text-blue-700 font-medium" href="#">About me</a>
-        </li>
-        <li className="block my-4 text-center text-sm">
-            <a className="uppercase text-blue-700 font-medium" href="#">Contact me</a>
-        </li>
+            {
+                navLinks.map( (link) => (
+                    <li key={link.id} className="block my-4 text-center text-sm">
+                        <a className="uppercase text-blue-700 font-medium" href={link.link}>{link.menu}</a>
+                    </li>
+                ))
+            }
         </ul>
     </>
   )
